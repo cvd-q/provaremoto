5174aeab7ac1df5c8ccd6c16885cdc10a80a4410
cvd <mjiawei@yahoo.it>
2020-09-09 17:43:23 +0200

    rimerge3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 // Your First C++ Program
 // modifica 1
 // modifica 2 b1
+<<<<<<< HEAD
 // MMMERGEEEEE??
+>>>>>>> master3
+<<<<<<< HEAD
+=======
 // modifica 3 b2
+>>>>>>> b2
 #include <iostream>
 
 int main() {
