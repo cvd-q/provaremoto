5673b6b24b19ca43b985738d7b448afba3338eb9
cvd <mjiawei@yahoo.it>
2020-09-10 22:46:23 +0200

    6

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-print("ciao mondo")
-print("ciao ciao")
-print("mondo mondo")
+print("ciao mondo1")
+print("ciao ciao2")
+print("mondo mondo3")
 print("cvd")
 <<<<<<< HEAD
 print("ciao cvd.......")
 >>>>>>> 7eaa6c075811edad3d7ee21953c25fcf811b0e39
+uno
+
+
+
